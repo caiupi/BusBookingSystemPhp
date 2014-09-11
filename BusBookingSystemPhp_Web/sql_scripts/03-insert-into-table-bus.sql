28e3fc46508dbac5de9fad18d88483a4241e19e5
Paul Davies <pd_account_2@hotmail.com>
2014-09-11 16:26:57 +0100

    Added database functionality.

---
+++ BusBookingSystemPhp_Web/sql_scripts/03-insert-into-table-bus.sql
@@ -0,0 +1,3 @@
+INSERT INTO `busbookingsystem`.`bus` (`id`, `name`, `description`) VALUES
+('281EB916-B59E-4F59-9D1F-757F3774BDB7', 'Bertha', '1979 Super Viking'),
+('3433676F-F9EC-4B2E-AAD9-9149342C915C', 'Thurston', '1969 Westfalia');
